f9c50bf12c08def4eaada5994285e79329250ce3
Elyas <86181794+Paquito1923@users.noreply.github.com>
2021-12-31 22:18:11 +0700

    Wkwk

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -6,12 +6,12 @@ if (!args[2]) throw 'masukkan nama branch'
 let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/${args[2]}.zip`
 //F
 m.reply(`compressing data to file zip*`)
-conn.sendFile( m.chat, url, url, null, m)
+conn.sendFile( m.chat, url, `${args[1]} ${args[2]}.zip`, null, m)
 
 }
-handler.help = ['githubdl']
-handler.tags = ['github']
-handler.command = /githubdl/i
+handler.help = ['ghdl']
+handler.tags = ['download']
+handler.command = /ghdl/i
 
 handler.limit = true
 
