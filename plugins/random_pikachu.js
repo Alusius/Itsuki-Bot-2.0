9a10ea348730f4730ac6b4b8f06f514164a65a95
Elyas <86181794+Paquito1923@users.noreply.github.com>
2021-12-31 11:22:58 +0700

    Update random_pikachu.js

--- plugins/random_pikachu.js
+++ plugins/random_pikachu.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, text }) => {
     let json = await res.json()
     if (json.status) throw json
     let caption = `
-©BakaBOTZ
+Pikacu
 `.trim()
     conn.sendFile(m.chat, json.link, '', caption, m)
    } catch (e) {
