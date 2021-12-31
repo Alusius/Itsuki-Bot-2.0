dd797d7b42b78d1f5576efdcebb6efe56d638393
Elyas <86181794+Paquito1923@users.noreply.github.com>
2021-12-31 10:08:33 +0700

    Create waalaikummussalam.js

---
+++ plugins/waalaikummussalam.js
@@ -0,0 +1,54 @@
+let moment = require('moment-timezone')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+let fs = require('fs')
+let handler = async (m, {conn}) => {
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let anu = `وَعَلَيْكُمْ السَّلاَمُ
+Halooo👋`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: anu,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/waalaikumussalam.jpg') }, 
+           hydratedFooterText: `Stay halal`,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '📍instagram',
+               url: instagram
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Menu',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+handler.customPrefix = /^ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم$/i // ketik bot (tanpa prefix)
+handler.command = new RegExp
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+module.exports = handler
