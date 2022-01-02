626c6ccb4c37add6c1f3b97b8061c3d473cb5bbc
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-02 14:04:03 +0700

    Add files via upload

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -1,7 +1,7 @@
-let fetch = require('node-fetch')
 
+const fetch = require('node-fetch')
 let timeout = 120000
-let poin = 500
+let poin = 2500
 let handler = async (m, { conn, usedPrefix }) => {
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
@@ -9,21 +9,19 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.asahotak[id][0])
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/asahotak', {}, 'APIKEY'))
-    if (res.status !== 200) throw await res.text()
-    let json = await res.json()
-    if (!json.status) throw json
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/asahotak.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
-${json.result.soal}
+${json.soal}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}ao untuk bantuan
 Bonus: ${poin} XP
-`.trim()
+    `.trim()
     conn.asahotak[id] = [
-        await conn.reply(m.chat, caption, m),
+        await conn.sendButton(m.chat, caption, 'By elyas', 'Bantuan', '.ao', m),
         json, poin,
-        setTimeout(() => {
-            if (conn.asahotak[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, conn.asahotak[id][0])
+        setTimeout(async () => {
+            if (conn.asahotak[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, 'By Elyas', 'Asah Otak', '.asahotak', conn.asahotak[id][0])
             delete conn.asahotak[id]
         }, timeout)
     ]
--- plugins/asahotak_ans.js
+++ plugins/asahotak_ans.js
@@ -3,18 +3,18 @@ const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.contentText)) return !0
     this.asahotak = this.asahotak ? this.asahotak : {}
     if (!(id in this.asahotak)) return m.reply('Soal itu telah berakhir')
     if (m.quoted.id == this.asahotak[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
-        // m.reply(JSON.stringify(json, null, '\t'))
-        if (m.text.toLowerCase() == json.result.jawaban.toLowerCase().trim()) {
+        if (['.ao', 'Bantuan', ''].includes(m.text)) return !0
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.asahotak[id][2]
-            m.reply(`*Benar!*\n+${this.asahotak[id][2]} XP`)
+            await this.sendButton(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`, 'By elyas', 'Asah Otak', '.asahotak', m)
             clearTimeout(this.asahotak[id][3])
             delete this.asahotak[id]
-        } else if (similarity(m.text.toLowerCase(), json.result.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
         else m.reply(`*Salah!*`)
     }
     return !0
