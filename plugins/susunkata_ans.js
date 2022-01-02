244abdb945d7f6f099e6254a9b2fd4bff6f97474
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-02 18:45:36 +0700

    Add files via upload

--- plugins/susunkata_ans.js
+++ plugins/susunkata_ans.js
@@ -1,36 +1,24 @@
-let fetch = require('node-fetch')
+const similarity = require('similarity')
+const threshold = 0.72
 
-let timeout = 120000
-let poin = 500
-let handler = async (m, { conn, usedPrefix }) => {
-    conn.susunkata = conn.susunkata ? conn.susunkata : {}
+let handler = m => m
+
+handler.before = async function (m) {
     let id = m.chat
-    if (id in conn.susunkata) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.susunkata[id][0])
-        throw false
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*suka/i.test(m.quoted.contentText)) return !0
+    this.susunkata = this.susunkata ? this.susunkata : {}
+    if (!(id in this.susunkata)) return m.reply('Susun Kata telah berakhir')
+    if (m.quoted.id == this.susunkata[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.susunkata[id][1]))
+        if (['.suka', ''].includes(m.text)) return !0
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
+            db.data.users[m.sender].exp += this.susunkata[id][2]
+            await this.sendBut(m.chat, benar + ` +${this.susunkata[id][2]} XP`, wm, 'Susun Kata', '.susunkata', m)
+            clearTimeout(this.susunkata[id][3])
+            delete this.susunkata[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(dikit)
+        else m.reply(salah)
     }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')
-    if (!res.ok) throw eror
-    let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
-    let caption = `
-${json.soal}
-Tipe: ${json.tipe}
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}suka untuk bantuan
-Bonus: ${poin} XP
-`.trim()
-    conn.susunkata[id] = [
-        await conn.sendBut(m.chat, caption, wm, 'Bantuan', '.suka'),
-        json, poin,
-        setTimeout(async () => {
-            if (conn.susunkata[id]) await conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, 'Susun Kata', '.susunkata')
-            delete conn.susunkata[id]
-        }, timeout)
-    ]
+    return !0
 }
-handler.help = ['susunkata']
-handler.tags = ['game']
-handler.command = /^susunkata/i
-
 module.exports = handler
