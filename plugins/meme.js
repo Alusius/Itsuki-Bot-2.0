c658b8fee09f60dca6e15e0be2e817a2207ccd4e
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-02 22:41:15 +0700

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
     let url = global.API('xteam', '/randomimage/meme', {}, 'APIKEY')
-    await conn.sendButtonImg(m.chat, url, '*MEME*', wm, 'NEXT', '.memes',m, 0, { thumbnail: await (await fetch(url)).buffer() })
+    await conn.sendButtonImg(m.chat, url, '*MEME*', wm, 'NEXT', '.meme',m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
 handler.help = ['meme']
 handler.tags = ['fun']
