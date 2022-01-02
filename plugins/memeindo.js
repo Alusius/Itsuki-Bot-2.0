901034ba3bab058d983273e30e7bdb2a29fe6d36
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-02 13:01:28 +0700

    Add files via upload

--- plugins/memeindo.js
+++ plugins/memeindo.js
@@ -2,11 +2,10 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=caliph_71')
   let barbar = await url.json()
-await conn.sendButtonImg(m.chat, barbar.result, '*MEME INDO*', wm, 'NEXT', '.memeindo',m, 0, { thumbnail: await (await fetch(url)).buffer() })
+conn.sendFile(m.chat, barbar.result, 'memeindo','*MEME INDO*', m)
 }
-handler.help = ['memeindo']
-handler.tags = ['fun']
 handler.command = /^(memeindo)$/i
+handler.private = false
 handler.limit = false
 
 module.exports = handler
