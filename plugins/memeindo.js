a7fb6980aab10ade01623d8cce23259cd25f05f6
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-02 12:57:25 +0700

    Update memeindo.js

--- plugins/memeindo.js
+++ plugins/memeindo.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
   let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=caliph_71')
   let barbar = await url.json()
-await conn.sendButtonImg(m.chat, barbar.result, '*MEME INDO*', wm, 'NEXT', '.memes',m, 0, { thumbnail: await (await fetch(url)).buffer() })
+await conn.sendButtonImg(m.chat, barbar.result, '*MEME INDO*', wm, 'NEXT', '.memeindo',m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
 handler.help = ['memeindo']
 handler.tags = ['fun']
