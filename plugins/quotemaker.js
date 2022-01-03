1d03586874fd5f54addfae488d2b08f05d82cd33
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-03 19:13:12 +0700

    Update quotemaker.js

--- plugins/quotemaker.js
+++ plugins/quotemaker.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
   await conn.sendFile(m.chat, global.API('xteam', '/quotemaker', { text: teks, wm: wm ? wm : conn.getName(m.sender) }, 'APIKEY'), 'file.jpg', 'Nih', m)
 }
 handler.help = ['quote'].map(v => v + 'maker <teks>|<wm>')
-handler.tags = ['editor']
+handler.tags = ['maker']
 handler.command = /^quotemaker$/i
 
 handler.limit = true
