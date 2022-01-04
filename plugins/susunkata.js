19d8b35acc9e519befe3a5a9beb43bb5f4755108
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-04 08:32:16 +0700

    Update susunkata.js

--- plugins/susunkata.js
+++ plugins/susunkata.js
@@ -1,15 +1,18 @@
 let fetch = require('node-fetch')
+
 let timeout = 120000
 let poin = 500
-
 let handler = async (m, { conn, usedPrefix }) => {
     conn.susunkata = conn.susunkata ? conn.susunkata : {}
     let id = m.chat
-    if (id in conn.susunkata) return conn.reply(m.chat, 'Belum dijawab!', conn.susunkata[id][0])
-    let res = await fetch(API('amel', '/susunkata', {}, 'apikey'))
+    if (id in conn.susunkata) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.susunkata[id][0])
+        throw false
+    }
+    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/susunkata.json')
     if (!res.ok) throw eror
-    let json = await res.json()
-    if (!json.status) throw json
+    let data = await res.json()
+    let json = data[Math.floor(Math.random() * data.length)]
     let caption = `
 ${json.soal}
 
@@ -17,12 +20,13 @@ Tipe: ${json.tipe}
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}suka untuk bantuan
+Bonus: ${poin} XP
 `.trim()
     conn.susunkata[id] = [
-        await conn.sendBut(m.chat, caption, wm, 'Bantuan', '.suka', m),
+        await conn.sendBut(m.chat, caption, wm, 'Bantuan', '.suka'),
         json, poin,
-        setTimeout(() => {
-            if (conn.susunkata[id]) conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, 'Susun Kata', '.susunkata', conn.susunkata[id][0])
+        setTimeout(async () => {
+            if (conn.susunkata[id]) await conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, 'Susun Kata', '.susunkata')
             delete conn.susunkata[id]
         }, timeout)
     ]
@@ -30,7 +34,5 @@ Ketik ${usedPrefix}suka untuk bantuan
 handler.help = ['susunkata']
 handler.tags = ['game']
 handler.command = /^susunkata/i
-
-handler.game = true
 
 module.exports = handler
