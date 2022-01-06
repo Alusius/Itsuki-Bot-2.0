3fa5a5b34e3340989ddc732202008ffe8453d4ff
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-06 11:41:53 +0700

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -6,18 +6,18 @@ let handler  = async (m, { conn, args, text }) => {
   if (!text) return m.reply('Cari apa?')
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
-  if (!url) return m.reply('404 Not Found')
-  conn.sendFile(m.chat, url, 'gimage', `
+  if (!url) return m.reply('Not Found')
+  conn.sendButtonImg(m.chat, url, `
 *── 「 GOOGLE IMAGE 」 ──*
 
 ${text}
 ➸ *width*: ${width}
 ➸ *height*: ${height}
-`.trim(), m)
+`.trim(), wm, NEXT, `.gimage ${text}, m)
 }
-handler.help = ['image <query>']
+handler.help = ['image <query>', 'gimage <query>', 'googleimage <query>']
 handler.tags = ['internet']
-handler.command = /^(gimage|image)$/i
+handler.command = /^(gimage|googleimage|image)$/i
 
 module.exports = handler
 
