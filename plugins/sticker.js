95d0f1f7273d7ae756fce01d8db54cd3e6a64134
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-10 19:32:36 +0700

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn }) => {
             if (!img) throw `reply image with command s`
             stiker = await sticker5(img, false, packname, author)
         } else if (/video/.test(mime)) {
-            if ((q.msg || q).seconds > 11) return m.reply('max is 10 seconds!')
+            if ((q.msg || q).seconds > 11) return m.reply('maksimal 10 detik!')
             let img = await q.download()
             if (!img) throw `reply video with command s`
             stiker = await sticker5(img, false, packname, author)
@@ -27,8 +27,8 @@ let handler = async (m, { conn }) => {
     }
     finally {
         if (stiker) {
-          m.reply(stiker_wait)
-            await conn.sendFile(m.chat, stiker, 'stiker.webp', '', m)
+            m.reply(stiker_wait)
+            await conn.sendFile(m.chat, stiker, '', '', m)
         }
         else {
 
