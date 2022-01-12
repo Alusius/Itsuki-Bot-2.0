d2d796b7c273d75eb2ef2b1faaeed434b3b71dea
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-13 05:51:48 +0700

    Update sadboylogo.js

--- plugins/sadboylogo.js
+++ plugins/sadboylogo.js
@@ -1,9 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-   if (!args[0]) return m.reply('masukkan angka')
-       if (!args[1]) return m.reply('masukkan angka lagi')
-  m.reply('*Wait Ngab*\nProses...')
+   if (!args[0]) return m.reply('masukkan text1 dan text2\n\nContok: .sadboylogo elyas|ganzz')
+   m.reply('*Wait Ngab*\nProses...')
   let res = `https://melcanz.com/sadboy?nama=${response[0]}&nama2=${response[1]}&apikey=dUtJxxvp`
   conn.sendFile(m.chat, res, 'sadboy.jpg', `Sudah Jadi`, m, false)
 }
