b3789b194eb122b3aec2dd49af4966dadea064e6
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-14 07:41:51 +0700

    Update apakah.js

--- plugins/apakah.js
+++ plugins/apakah.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  m.reply(m.chat, `${pickRandom(['Ho oh','Sepertinya Begitu','Kayaknya','Kayaknya nggak','Nggak','Nggak mungkin'])}
+  conn.reply(m.chat, `${pickRandom(['Yap','Sepertinya Begitu','Kayaknya','Kayaknya nggak','Nggak','Nggak mungkin'])}
 `.trim(), m, m.mentionedJid ? {
   contextInfo: {
     mentionedJid: m.mentionedJid
