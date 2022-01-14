3835c944d5ac9f0c94eb6547a5082f8fd10fd5dc
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-14 07:17:27 +0700

    Create elyasganteng.js

---
+++ plugins/elyasganteng.js
@@ -0,0 +1,10 @@
+let fs = require('fs')
+let handler = async (m) => {
+let stc = fs.readFileSync('./media/boo.webp')
+conn.sendFile(m.chat, stc, 0, 0, m)
+}
+
+handler.customPrefix = /^(sendal)$/i
+handler.command = new RegExp
+
+module.exports = handler
