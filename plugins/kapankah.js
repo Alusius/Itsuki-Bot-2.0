527c82787df99b4a40ddc9dffd7fc6f598998aea
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-15 15:29:09 +0700

    Update kapankah.js

--- plugins/kapankah.js
+++ plugins/kapankah.js
@@ -1,7 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  conn.reply(m.chat, `
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${Math.floor(Math.random() * 10)} ${pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])} lagi ...
+  conn.reply(m.chat, `Kayaknya ${Math.floor(Math.random() * 10)} ${pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'abad'])} lagi ...
 `.trim(), m, m.mentionedJid ? {
   contextInfo: {
     mentionedJid: m.mentionedJid
