0a33a5b11d560b969d9543683d589f440308271d
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-21 10:45:21 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -82,7 +82,7 @@ let handler = async(m,{text, conn}) => {
 
 let p = await  hxz.ttdownloader(text)
 const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, nowm, m)
+conn.sendFile(m.chat, nowm, null, 'nih', m)
 }
 handler.command = ['ttdl']
 module.exports = handler
