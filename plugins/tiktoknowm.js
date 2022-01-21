6208490c716ecc278c4a4c8cabea8df2c6ac7926
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-21 14:16:07 +0700

    Update tiktoknowm.js

--- plugins/tiktoknowm.js
+++ plugins/tiktoknowm.js
@@ -77,7 +77,7 @@ let handler = async(m,{text, conn}) => {
 
 let p = await  hxz.ttdownloader(text)
 const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, nowm, m)
+conn.sendFile(m.chat, nowm, null, 'nih', m)
 }
 handler.command = ['tiktoknowm']
 module.exports = handler
