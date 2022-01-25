5f97346acfdabf8aabc6345a284316435f09a8db
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-25 14:31:38 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -460,7 +460,7 @@ module.exports = {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', '@' + user.split('@')[0])
-                                this.sendButtonLoc(id, text, await(await fetch(pp)).buffer, wm, "ok", "huuu", null)
+                                this.sendButtonLoc(id, await(await fetch(pp)).buffer(), text, 'F', "ok", ".menu", null)
                                 }
                     }
                 }
