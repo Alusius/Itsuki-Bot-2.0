642d8009f33a9ee096b4865d3b3476ea091f19f1
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-25 09:08:46 +0700

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -3,41 +3,39 @@ let handler = async (m, { conn, participants, groupMetadata }) => {
     const getGroupAdmins = (participants) => {
         admins = []
         for (let i of participants) {
-            i.isAdmin ? admins.push(i.jid) : ''
+            i.admin === "admin" ? admins.push(i.id) : ''
         }
         return admins
     }
 
     let pp = './src/avatar_contact.png'
     try {
-        pp = await conn.getProfilePicture(m.chat)
+        pp = await conn.profilePictureUrl(m.chat, 'image')
     } catch (e) {
     } finally {
         let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink } = global.db.data.chats[m.chat]
         const groupAdmins = getGroupAdmins(participants)
         let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
         let text = `*「 TAG ADMIN 」*\n
-*Nama Grup:* 
+
+*Name:* 
 ${groupMetadata.subject}
 
-*Pembuat Group:* 
+*Group Owner:* 
 @${m.chat.split`-`[0]}
 
 *Group Admins:*
 ${listAdmin}
-
 `.trim()
         ownernya = [`${m.chat.split`-`[0]}@s.whatsapp.net`]
         let mentionedJid = groupAdmins.concat(ownernya)
         conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, { contextInfo: { mentionedJid } })
     }
 }
-handler.help = ['tagadmin']
+handler.help = ['infogrup']
 handler.tags = ['group']
-handler.command = /^(tagadmin)$/i
+handler.command = /^(gro?upinfo|info(gro?up|gc))$/i
 
 handler.group = true
-
-handler.limit = true
 
 module.exports = handler
