552e9fe9d2c24563e8d87cddc592bc581f24a255
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-25 13:25:59 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 const simple = require('./lib/simple')
 const util = require('util')
+const fetch = require('node-fetch')
+const fs = require('fs')
 
 const isNumber = x => typeof x === 'number' && !isNaN(x)
 const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(resolve, ms))
