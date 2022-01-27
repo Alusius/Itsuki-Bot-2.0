84c2da6c3f430629eaeb6465bb2ba7daf0182464
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-27 19:24:23 +0700

    Update ffmpeg_toptt.js

--- plugins/ffmpeg_toptt.js
+++ plugins/ffmpeg_toptt.js
@@ -1,40 +1,22 @@
 const { toPTT } = require('../lib/converter')
-const { MessageType } = require('@adiwajshing/baileys')
-let fs = require('fs')
-let path = require('path')
-let fetch = require('node-fetch')
-let moment = require('moment-timezone')
-const Elys = {
-	key : {
-                          participant : '0@s.whatsapp.net'
-                        },
-       message: {
-                    orderMessage: {
-                            itemCount : 1,
-                            status: 990,
-                            surface : 1,
-                            message: `To Vn`, //Kasih namalu
-                            orderTitle: 'ig: elyas_tzy',
-                            thumbnail: fs.readFileSync('./src/Kurumiloli.png'), //Gambarnye
-                            sellerJid: '0@s.whatsapp.net'
-          
-                          }
-                        }
-                      }
+const { MessageType } = require('@adiwajshing/baileys-md')
+
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Balas audio yang ingin diubah ke voice note dengan caption *${usedPrefix + command}*`
+  if (!/video|audio/.test(mime)) throw `Balas video atau voice note yang ingin diubah ke mp3 dengan caption *${usedPrefix + command}*`
   let media = await q.download()
   let audio = await toPTT(media, 'mp4')
-  conn.sendMessage(m.chat, audio, MessageType.audio, {
-    quoted: Elys,
+ 
+//conn.sendFile(m.chat, audio,  0,0,m,true, {ptt:true })
+ conn.sendMessage(m.chat, {audio:audio}, {
+    quoted: m,
     ptt: true
   })
 }
-handler.help = ['tovn (reply)']
+handler.help = ['tomp3 (reply)']
 handler.tags = ['audio']
 
-handler.command = /^to(vn|(ptt)?)$/i
+handler.command = /^to(mp3|a(udio)?)$/i
 
 module.exports = handler
