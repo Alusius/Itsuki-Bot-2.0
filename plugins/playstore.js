f22141aa3a5bea1b971f792a3733e9aeadfa5a1a
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-28 06:25:23 +0700

    Update playstore.js

--- plugins/playstore.js
+++ plugins/playstore.js
@@ -31,3 +31,22 @@ handler.command = /^(playstore)$/i
 handler.limit = true
 
 module.exports = handler
+
+/*let hxz = require('hxz-api')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text }) => {
+    if (!text) throw 'Masukkan Link\n\nContoh: .playstore coc'
+  let res = await hxz.playstore(text)
+conn.sendBut(m.chat, `*PLAY STORE*
+
+NAMA: ${res[0].name}
+DEVELOPER: ${res[0].developer}
+LINK: ${res[0].link}`, wm, 'ok', 'huuu', m)
+
+}
+handler.help = ['playstore' <apk>]
+handler.tags = ['internet']
+handler.command = /^playstore$/i
+
+
+module.exports = handler/*
