44dc59739b5676153eaebe7ceadc97bc902c59d7
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-01-29 10:25:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -42,13 +42,12 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'stickerby'
-global.author = `elyas
-(bot)
+global.author = `elyas(bot)
 
 
 
 
-ig:elyas_tzy`
+ig: ${instagram}`
 
 global.wm = 'by elyas'
 global.media = 'https://telegra.ph/file/ccb2359d3925fd7af58cd.jpg'
