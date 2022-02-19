6da242bad37f6ea051fb7847a24fd74aa82634cc
Elyas <86181794+Paquito1923@users.noreply.github.com>
2022-02-19 11:08:40 +0700

    Delete plugins directory

--- plugins/INFOBOT.js
+++
@@ -1,1 +0,0 @@
-global.informasibot = [`│`]
--- plugins/darkjoke.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'), 'Dark Jokes', wm, 'NEXT', '.darkjokes', m)
-}
-handler.help = ['darkjokes']
-handler.tags = ['internet']
-handler.command = /^(dragjokes|darkjokes)$/i
-
-module.exports = handler
