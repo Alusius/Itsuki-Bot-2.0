f9bef875e3671f48dcabcd1e9dc6d2d3c9e010b7
Hyzer <98345064+Hyzerr@users.noreply.github.com>
2022-04-10 09:02:59 +0700

    Update limit.js

--- plugins/limit.js
+++ plugins/limit.js
@@ -2,7 +2,18 @@ let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-    conn.sendBut(m.chat, `${global.db.data.users[who].limit} Limit Tersisaಥ_ಥ`, wm, 'BELI', '.buy', m)
+    fdoc = {
+  key : {
+  remoteJid: 'status@broadcast',
+  participant : '0@s.whatsapp.net'
+  },
+  message: {
+  documentMessage: {
+  title: wm, 
+                            }
+                          }
+                        }
+conn.sendBut(m.chat, `${global.db.data.users[who].limit} Limit Tersisaಥ_ಥ`, wm, 'BELI', '.buy', fdoc)
 }
 handler.help = ['limit [@user]']
 handler.tags = ['xp']
